85bfc009bb2f8fb608d63724ba60dc31e5a0bddb
Yolanda Robla <yroblamo@redhat.com>
2017-10-30 17:03:17 +0100

    Add new task for rebooting controllers after upgrade

---
+++ files/controller_reboot.sh
@@ -0,0 +1,42 @@
+for NODE in `nova list | grep controller | awk -F "|" '{ print $2 }'` ; do
+    NODE_IP=$(openstack server show $NODE -f json  | jq -r .addresses | grep -oP '[0-9.]+')
+    NODE_NAME=$(openstack server show $NODE -f json  | jq -r .name)
+
+    echo "Rebooting $NODE_NAME"
+    nova reboot $NODE_NAME
+
+    timeout_seconds=480
+    elapsed_seconds=0
+    while true; do
+        echo "Waiting for $NODE_NAME to go down ..."
+        NODE_DOWN=$(sudo ping -c1 $NODE_IP)
+        EXIT_STATUS=$?
+        if [ $EXIT_STATUS != 0 ]; then
+            break
+        fi
+        sleep 3
+        (( elapsed_seconds += 3 ))
+        if [ $elapsed_seconds -ge $timeout_seconds ]; then
+            echo "FAILURE: Node $NODE_NAME didn't reboot in time"
+            exit 1
+        fi
+    done
+
+    ## wait for node to get back online
+    elapsed_seconds=0
+    while true; do
+        echo "Waiting for $NODE boot ..."
+        PCS_STATUS=$(ssh -q -o StrictHostKeyChecking=no heat-admin@$NODE_IP 'sudo pcs status' | grep ^Online)
+        echo "status $PCS_STATUS"
+        if [[ $PCS_STATUS == *$NODE_NAME* ]]; then
+            break
+        fi
+        sleep 3
+        (( elapsed_seconds += 3 ))
+        if [ $elapsed_seconds -ge $timeout_seconds ]; then
+            echo "FAILURE: $NODE_NAME didn't come back up as part of the cluster"
+            exit 1
+        fi
+    done
+done
+
