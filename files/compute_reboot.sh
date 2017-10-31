ebd3515dffed506f8d26b08b8ae7e8dae54471fc
Yolanda Robla <yroblamo@redhat.com>
2017-10-31 16:26:01 +0100

    Add steps for upgrading compute nodes

--- files/compute_reboot.sh
+++ files/compute_reboot.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-for NODE in `nova list | grep ceph | awk -F "|" '{ print $2 }'` ; do
+for NODE in `nova list | grep compute | awk -F "|" '{ print $2 }'` ; do
     NODE_IP=$(openstack server show $NODE -f json  | jq -r .addresses | grep -oP '[0-9.]+')
     NODE_NAME=$(openstack server show $NODE -f json  | jq -r .name)
-
-    # disable ceph storage cluster rebalancing temporarily
-    ssh -q -o StrictHostKeyChecking=no heat-admin@$NODE_IP 'sudo ceph osd set noout'
-    ssh -q -o StrictHostKeyChecking=no heat-admin@$NODE_IP 'sudo ceph osd set norebalance'
 
     echo "Rebooting $NODE_NAME"
     nova reboot $NODE_NAME
@@ -30,22 +26,17 @@ for NODE in `nova list | grep ceph | awk -F "|" '{ print $2 }'` ; do
     ## wait for node to get back online
     elapsed_seconds=0
     while true; do
-        echo "Waiting for $NODE_NAME boot ..."
-        CEPH_STATUS=$(ssh -q -o StrictHostKeyChecking=no heat-admin@$NODE_IP 'exit')
-        EXIT_STATUS=$?
-        echo "exit is $EXIT_STATUS"
-        if [ $EXIT_STATUS == 0 ]; then
+        echo "Waiting for $NODE boot ..."
+        COMPUTE_STATUS=$(ssh -q -o StrictHostKeyChecking=no heat-admin@$NODE_IP 'exit')
+        EXIT_CODE=$?
+        if [[ $EXIT_CODE == 0 ]]; then
             break
         fi
         sleep 3
         (( elapsed_seconds += 3 ))
         if [ $elapsed_seconds -ge $timeout_seconds ]; then
-            echo "FAILURE: Node $NODE_NAME didn't reboot in time"
+            echo "FAILURE: $NODE_NAME didn't come back up as part of the cluster"
             exit 1
         fi
     done
 done
-
-# login into the ceph node, and rebalance the cluster
-ssh -q -o StrictHostKeyChecking=no heat-admin@$NODE_IP 'sudo ceph osd unset noout'
-ssh -q -o StrictHostKeyChecking=no heat-admin@$NODE_IP 'sudo ceph osd unset norebalance'
